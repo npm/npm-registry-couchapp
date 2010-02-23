2a955dff3f1a089411ca66edc044943443b261c5
isaacs <i@izs.me>
2010-02-24 07:12:11 +0800

    DRY up the show function a little bit.

--- design/shows/package.js
+++ design/shows/package.js
@@ -1,26 +1,23 @@
 function (doc, req) {
+  var code = 200,
+    headers = {"Content-Type":"application/json"},
+    body = null;
   if (req.query.version) {
-    var code = 200;
     if (isNaN(parseInt(req.query.version[0]))) {
-      var v = doc.versions[doc['dist-tags'][req.query.version]]
+      body = doc.versions[doc['dist-tags'][req.query.version]]
     } else {
-      var v = doc.versions[req.query.version]
+      body = doc.versions[req.query.version]
     }
-    if (v == undefined) {
+    if (!body) {
       code = 404;
-      v = {"error" : "version not found: "+req.query.version};
+      body = {"error" : "version not found: "+req.query.version};
     }      
-    var response = {
-      code : code,
-      body : toJSON(v),
-      headers : {"Content-Type":"application/json"},
-    };
-    return response; 
   } else {
-    var response = {
-      body : toJSON(doc),
-      headers : {"Content-Type":"application/json"},
-    };
-    return response;
+    body = doc;
   }
+  return {
+    code : code,
+    body : toJSON(body),
+    headers : headers,
+  };
 }
