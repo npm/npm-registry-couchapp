9cecc8250b30d75e188482142d9ceeaedcb423d2
isaacs <i@izs.me>
2010-02-24 07:07:05 +0800

    Return 404 when package version is not found.  Pretty-print JSON.

--- design/shows/package.js
+++ design/shows/package.js
@@ -1,21 +1,26 @@
 function (doc, req) {
   if (req.query.version) {
+    var code = 200;
     if (isNaN(parseInt(req.query.version[0]))) {
       var v = doc.versions[doc['dist-tags'][req.query.version]]
     } else {
       var v = doc.versions[req.query.version]
     }
     if (v == undefined) {
-      // 404
+      code = 404;
+      v = {"error" : "version not found: "+req.query.version};
     }      
-    var response = { body:toJSON(v),
-                     headers:{"Content-Type":"application/json"},
-                    };
+    var response = {
+      code : code,
+      body : toJSON(v),
+      headers : {"Content-Type":"application/json"},
+    };
     return response; 
   } else {
-    var response = { body:toJSON(doc),
-                     headers:{"Content-Type":"application/json"},
-                    };
+    var response = {
+      body : toJSON(doc),
+      headers : {"Content-Type":"application/json"},
+    };
     return response;
   }
 }
