aa9bf050ab0365129473f86926e71710649451eb
isaacs <i@izs.me>
2010-02-24 08:24:23 +0800

    Remove promises.  Much reduced code size.

--- sync.js
+++ sync.js
@@ -1,74 +1,32 @@
-var couchdb = require('../node-couchdb/lib/couchdb');
-var fs = require('fs');
-var sys = require('sys');
-var url = require('url');
 
-var ddoc = {_id:"_design/app",shows:{},updates:{}}
+var couchdb = require('./deps/node-couchdb/lib/couchdb'),
+  fs = require('fs'),
+  sys = require('sys'),
+  url = require('url'),
 
-var argumentsToArray = function (args) {
-  var newargs = []
-  for (i=0;i<args.length;i+=1) {
-    newargs.push(args[i]);
-  }
-  return newargs;
-}
+  ddoc = {_id:"_design/app",shows:{},updates:{}},
 
-function Promise () {
-  var p = this;
-  this.addListener("error", function () {
-    if (p.listeners("error").length == 1) {
-      throw "Unhandled error event."
+  dev = false,
+  dburl = "http://localhost:5984/jsregistry",
+
+   // filename : cb
+  files = {
+    "design/rewrites.json" : function (data) {
+      ddoc.rewrites = JSON.parse(data);
+    },
+    "design/shows/package.js" : function (data) {
+      ddoc.shows.package = data;
+    },
+    "design/updates/package.js" : function (data) {
+      ddoc.updates.package = data;
     }
-  })
-}
-sys.inherits(Promise, process.EventEmitter)
-Promise.prototype.addCallback = function (callback) {
-  this.addListener("success", callback);
-}
-Promise.prototype.addErrback = function (callback) {
-  this.addListener("error", callback);
-}
-Promise.prototype.emitSuccess = function () {
-  var args = argumentsToArray(arguments);
-  args.unshift("success");
-  this.emit.apply(this, args);
-}
-Promise.prototype.emitError = function () {
-  var args = argumentsToArray(arguments);
-  args.unshift("error");
-  this.emit.apply(this, args);
-}
+  },
 
-function GroupPromise () {
-  this.promises = [];
-  this.successCount = 0;
-  this.failCount = 0;
-}
-GroupPromise.prototype.newPromise = function () {
-  var p = new Promise();
-  var gp = this;
-  p.addCallback(function () {
-    gp.successCount += 1; 
-    if (gp.whenFunc && gp.promises.length == (gp.failCount + gp.successCount)) {
-      gp.whenFunc();
-    }
-  });
-  p.addErrback(function () {gp.failCount += 1});
-  this.promises.push(p);
-  return p;
-}
-GroupPromise.prototype.when = function (callback) {
-  if (this.whenFunc && this.successCount.length == (this.failCount + this.successCount)) {
-    callback();
-  } else {
-    this.whenFunc = callback;
-  }
-}
-
-dev = false;
-dburl = "http://localhost:5984/jsregistry"
-
-for (i=0;i<process.argv.length;i+=1) {
+  // be gentle.
+  firstTime = true;
+  
+// parse args
+for (var i=0;i<process.argv.length;i+=1) {
   if (process.argv[i].slice(0,4) == 'http') {
     dburl = process.argv[i];
   }
@@ -77,43 +35,56 @@ for (i=0;i<process.argv.length;i+=1) {
   }
 }
 
-function attach (file, group, callback) {
-  var p = group.newPromise();
-  fs.readFile(file, undefined, function (error, data) {
-    callback(data)
-    if (dev) {
-      process.watchFile(file, {persistent: true, interval: 100}, sync);
-    }
-    p.emitSuccess();
-  });
-  return p;
-}
-
-var sync = function () {
-  var group = new GroupPromise();
-  attach('design/rewrites.json', group, function (data) {ddoc.rewrites = JSON.parse(data)})
-  attach('design/shows/package.js', group, function (data) {ddoc.shows.package = data})
-  attach('design/updates/package.js', group, function (data) {ddoc.updates.package = data})
-  group.when(function () {
-    var uri = url.parse(dburl)
-    var client = couchdb.createClient(uri.port, uri.hostname);
-    db = client.db(uri.pathname.replace('/', ''));
-    var p = db.getDoc('_design/app', function (error, doc) {
-      if (!error) {
-        ddoc._rev = doc._rev;
-      }
-      db.saveDoc(ddoc, function (error, info) {
-        if (error) {
-          sys.puts("Failed to save "+JSON.stringify(error))
-        } else {
-          sys.puts("Saved "+JSON.stringify(info))
-        }
-      })
-    })
-  }) 
-}
-
+// do it!
 sync();
 
+////// worker functions below.
 
+function sync () {
+  var saw = 0, expect = 0;
+  // extra function to trap the value of cb,
+  // so it's not changing the closed-over value.
+  for (var file in files) (function (file, cb) {
+    expect ++;
+    fs.readFile(file, function (er, data) {
+      if (er) throw er;
+      saw ++;
+      cb(data);
+      if (saw >= expect) done();
+    });
 
+    // if a file changes, then update that bit of the ddoc, and push it up.
+    if (dev && firstTime) {
+      process.watchFile(file, {persistent: true, interval: 100}, function (o, n) {
+        if (o.mtime.getTime() === n.mtime.getTime()) return;
+        sys.error("Changed: "+file);
+        fs.readFile(file, function (er, data) {
+          if (er) throw er;
+          cb(data);
+          done();
+        });
+      });
+    }
+  })(file, files[file]);
+  if (dev && firstTime) sys.error("Starting up in dev mode");
+  firstTime = false;
+}
+
+// this gets called when all the files get read
+function done () {
+  var uri = url.parse(dburl)
+  var client = couchdb.createClient(uri.port || 5984, uri.hostname || "localhost");
+  db = client.db((uri.pathname || "jsregistry").replace('/', ''));
+  db.getDoc('_design/app', function (error, doc) {
+    if (!error) {
+      ddoc._rev = doc._rev;
+    }
+    db.saveDoc(ddoc, function (error, info) {
+      if (error) {
+        sys.error("Failed to save "+JSON.stringify(error))
+      } else {
+        sys.error("Saved "+JSON.stringify(info))
+      }
+    });
+  });
+}
