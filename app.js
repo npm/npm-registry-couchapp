060377f040c1bd574ce77574f28c8a115893d420
isaacs <i@izs.me>
2010-03-24 01:33:21 -0700

    Add a document index listing out the packages in the registry and the available versions for each.

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
 var couchapp = require('couchapp');
 
-var ddoc = {_id:'_design/app', shows:{}, updates:{}};
+var ddoc = {_id:'_design/app', shows:{}, updates:{}, views:{}, lists:{}};
 exports.app = ddoc;
 
-ddoc.rewrites = [ 
+ddoc.rewrites = [
+  { from: "/", to:"_list/index/listAll", method: "GET" },
+
   { from: "/adduser/:user", to:"../../../_users/:user", method: "PUT" },
-  
+
   { from: "/:pkg", to: "/_show/package/:pkg", method: "GET" },
-  { from: "/:pkg/:version", to: "_show/package/:pkg", method: "GET", 
+  { from: "/:pkg/:version", to: "_show/package/:pkg", method: "GET",
     query: { version: ":version" }
   },
-  
+
   { from: "/:pkg", to: "/_update/package/:pkg", method: "PUT" },
-  { from: "/:pkg/:version", to: "_update/package/:pkg", method: "PUT", 
+  { from: "/:pkg/:version", to: "_update/package/:pkg", method: "PUT",
     query: { version : ":version" }
   },
-  
+
   { from: "/:pkg", to: "../../:pkg", method: "DELETE" },
 ]
+
+ddoc.lists.index = function (head, req) {
+  var row,
+    out = {};
+  while (row = getRow()) {
+    out[row.id] = Object.keys(row.value.versions);
+  }
+  send(toJSON(out));
+}
+ddoc.views.listAll = {
+  map : function (doc) { emit(doc.id, doc) }
+}
 
 ddoc.shows.package = function (doc, req) {
   var code = 200,
@@ -32,7 +46,7 @@ ddoc.shows.package = function (doc, req) {
     if (!body) {
       code = 404;
       body = {"error" : "version not found: "+req.query.version};
-    }      
+    }
   } else {
     body = doc;
   }
@@ -48,7 +62,7 @@ ddoc.updates.package = function (doc, req) {
   function error (reason) {
     return [{forbidden:reason}, JSON.stringify({forbidden:reason})];
   }
-  
+
   if (doc) {
     if (req.query.version) {
       var parsed = semver(req.query.version);
@@ -97,27 +111,27 @@ ddoc.updates.package = function (doc, req) {
 
 ddoc.validate_doc_update = function (newDoc, oldDoc, user) {
   var semver = /v?([0-9]+)\.([0-9]+)\.([0-9]+)([a-zA-Z-][a-zA-Z0-9-]*)?/;
-  
+
   if (newDoc._deleted === true) {
     // Allow document deletion, this eventually will need to do user validation.
     return true;
   }
-  
+
   function assert (ok, message) {
     if (!ok) throw {forbidden:message};
   }
-  
+
   // TODO: If the user doesn't have access to update this thing,
   // then throw an unauthorized error
-  
+
   // if the newDoc is an {error:"blerg"}, then throw that right out.
   // something detected in the _updates/package script.
   if (newDoc.forbidden) throw {forbidden:newDoc.forbidden};
-  
+
   // make sure all the dist-tags and versions are valid semver
   assert(newDoc["dist-tags"], "must have dist-tags");
   assert(newDoc.versions, "must have versions");
-  
+
   for (var i in newDoc["dist-tags"]) {
     assert(semver(newDoc["dist-tags"][i]),
       "dist-tag "+i+" is not a valid version: "+newDoc["dist-tags"][i]);
