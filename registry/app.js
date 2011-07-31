f62c106eacd2c1a62b872db977956ddba319146b
Filip Noetzel <filip@j03.de>
2011-07-31 18:19:32 +0200

    Fix "Object.keys is not a function" on gecko < 2.0
    Adds the Object.keys shim also, when there is
    Date.prototype.toISOString but not Object.keys
    (That is the case if CouchDB runs against a Gecko
    1.9.2 (Firefox 3)).

--- registry/app.js
+++ registry/app.js
@@ -818,11 +818,11 @@ ddoc.shows.package = function (doc, req) {
       return d.getTime()
       return [d.getTime(), d.toUTCString(), ds, ts, tz]
     }
-    Object.keys = Object.keys
-      || function (o) { var a = []
-                        for (var i in o) a.push(i)
-                        return a }
   }
+  Object.keys = Object.keys
+    || function (o) { var a = []
+                      for (var i in o) a.push(i)
+                      return a }
 
   var semver = require("semver")
     , code = 200
@@ -959,11 +959,11 @@ ddoc.updates.package = function (doc, req) {
       return d.getTime()
       return [d.getTime(), d.toUTCString(), ds, ts, tz]
     }
-    Object.keys = Object.keys
-      || function (o) { var a = []
-                        for (var i in o) a.push(i)
-                        return a }
   }
+  Object.keys = Object.keys
+    || function (o) { var a = []
+                      for (var i in o) a.push(i)
+                      return a }
 
   var semver = require("semver")
   var valid = require("valid")
@@ -1119,11 +1119,11 @@ ddoc.validate_doc_update = function (newDoc, oldDoc, user) {
       return d.getTime()
       return [d.getTime(), d.toUTCString(), ds, ts, tz]
     }
-    Object.keys = Object.keys
-      || function (o) { var a = []
-                        for (var i in o) a.push(i)
-                        return a }
   }
+  Object.keys = Object.keys
+    || function (o) { var a = []
+                      for (var i in o) a.push(i)
+                      return a }
 
   Array.isArray = Array.isArray
     || function (a) { return a instanceof Array
