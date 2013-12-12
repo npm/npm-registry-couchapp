0e6d4764d749d1db8b747f61ff6ad4c56176a7ae
Rod Vagg <rod@vagg.org>
2013-12-12 22:30:59 +1100

    allow for no auth and no rev

--- copy.sh
+++ copy.sh
@@ -39,10 +39,10 @@ rev=$(curl -k "$c"/_design/app | json _rev)
 if [ "$rev" != "" ]; then
   rev="?rev=$rev"
 fi
-auth="$(node -pe 'require("url").parse(process.argv[1]).auth || ""' "$c")"
+
+auth="$(node -pe 'a=require("url").parse(process.argv[1]).auth;a?"-u \""+a+"\"":""' "$c")"
 url="$(node -pe 'u=require("url");p=u.parse(process.argv[1]);delete p.auth;u.format(p)' "$c")"
 
-curl "$url/_design/scratch" \
-   -k -u "$auth" \
+curl -k $auth "$url/_design/scratch" \
   -X COPY \
   -H destination:'_design/app'$rev
