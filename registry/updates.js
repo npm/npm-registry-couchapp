21143e623e2b2838bd738ca3db97a37533ef8b56
isaacs <i@izs.me>
2014-01-21 10:24:29 -0800

    Update readme-trim

--- registry/updates.js
+++ registry/updates.js
@@ -26,7 +26,9 @@ updates.package = function (doc, req) {
   }
 
   var README_MAXLEN = 64 * 1024
+
   function readmeTrim(doc) {
+    var changed = false
     var readme = doc.readme || ''
     var readmeFilename = doc.readmeFilename || ''
     if (doc['dist-tags'] && doc['dist-tags'].latest) {
@@ -35,23 +37,30 @@ updates.package = function (doc, req) {
         readme = latest.readme
         readmeFilename = latest.readmeFilename || ''
       }
-      for (var v in doc.versions) {
-        // If we still don't have one, just take the first one.
-        if (doc.versions[v].readme && !readme)
-          readme = doc.versions[v].readme
-        if (doc.versions[v].readmeFilename && !readmeFilename)
-          readmeFilename = doc.versions[v].readmeFilename
-
-        delete doc.versions[v].readme
-        delete doc.versions[v].readmeFilename
-      }
     }
 
-    if (readme && readme.length > README_MAXLEN)
-      readme = readme.slice(0, README_MAXLEN)
+    for (var v in doc.versions) {
+      // If we still don't have one, just take the first one.
+      if (doc.versions[v].readme && !readme)
+        readme = doc.versions[v].readme
+      if (doc.versions[v].readmeFilename && !readmeFilename)
+        readmeFilename = doc.versions[v].readmeFilename
 
+      if (doc.versions[v].readme)
+        changed = true
+
+      delete doc.versions[v].readme
+      delete doc.versions[v].readmeFilename
+    }
+
+    if (readme && readme.length > README_MAXLEN) {
+      changed = true
+      readme = readme.slice(0, README_MAXLEN)
+    }
     doc.readme = readme
     doc.readmeFilename = readmeFilename
+
+    return changed
   }
 
   if (doc) {
