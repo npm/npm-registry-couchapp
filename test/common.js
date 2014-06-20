e3f206627e926316bfc076dd65918792b2e3c6d5
isaacs <i@izs.me>
2014-06-20 16:59:54 -0700

    Handle env.npm values that aren't exe-friendly (eg, .js on windows)

--- test/common.js
+++ test/common.js
@@ -1,9 +1,15 @@
 var which = require('which')
+
 exports.npmPath = process.env.npm || which.sync('npm')
 
 var spawn = require('child_process').spawn
 exports.npm = function (args, opts) {
-  return spawn(exports.npmPath, args, opts)
+  cmd = exports.npmPath
+  if (exports.npmPath.match(/\.js$/)) {
+    args = [exports.npmPath].concat(args)
+    cmd = process.execPath
+  }
+  return spawn(cmd, args, opts)
 }
 
 
